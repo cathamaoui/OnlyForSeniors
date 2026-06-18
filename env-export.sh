6ba6063f5e6fbdb5ba873a03d9a3575b65f38fc6
cathamaoui <cathamaoui@users.noreply.github.com>
2026-06-18 02:09:25 -0300

    Remove accidentally committed secrets

--- env-export.sh
+++
@@ -1,2 +0,0 @@
-export DATABASE_URL=postgresql://postgres.bfewbfvajrwiulcdtpuw:2026Peaches7351!@aws-1-us-east-1.pooler.supabase.com:6543/postgres
-export DIRECT_URL=postgresql://postgres:2026Peaches7351!@db.bfewbfvajrwiulcdtpuw.supabase.co:5432/postgres
